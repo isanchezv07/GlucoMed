a71b4c6cb7d008c7d0934b56b5a76dd77a135b46
IsanchezV <a01664141@tec.mx>
2024-03-07 20:10:27 -0600

    Update app

--- lib/main.dart
+++ lib/main.dart
@@ -1,23 +1,67 @@
-// ignore_for_file: unnecessary_new
-
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'LoginPage.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter/dashboard_screen.dart';
+import 'package:flutter/login_screen.dart';
+import 'package:flutter/transition_route_observer.dart';
 
-void main() async {
-  runApp(MyApp());
+void main() {
+  SystemChrome.setSystemUIOverlayStyle(
+    SystemUiOverlayStyle(
+      systemNavigationBarColor:
+          SystemUiOverlayStyle.dark.systemNavigationBarColor,
+    ),
+  );
+  runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget{
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
-  Widget build(BuildContext context){
-    return new MaterialApp(
-      title: 'Gluco Med',
-      theme: new ThemeData(
-        primarySwatch: Colors.blue,
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Login Demo',
+      theme: ThemeData(
+        textSelectionTheme:
+            const TextSelectionThemeData(cursorColor: Colors.orange),
+        // fontFamily: 'SourceSansPro',
+        textTheme: TextTheme(
+          displaySmall: const TextStyle(
+            fontFamily: 'OpenSans',
+            fontSize: 45.0,
+            // fontWeight: FontWeight.w400,
+            color: Colors.orange,
+          ),
+          labelLarge: const TextStyle(
+            // OpenSans is similar to NotoSans but the uppercases look a bit better IMO
+            fontFamily: 'OpenSans',
+          ),
+          bodySmall: TextStyle(
+            fontFamily: 'NotoSans',
+            fontSize: 12.0,
+            fontWeight: FontWeight.normal,
+            color: Colors.deepPurple[300],
+          ),
+          displayLarge: const TextStyle(fontFamily: 'Quicksand'),
+          displayMedium: const TextStyle(fontFamily: 'Quicksand'),
+          headlineMedium: const TextStyle(fontFamily: 'Quicksand'),
+          headlineSmall: const TextStyle(fontFamily: 'NotoSans'),
+          titleLarge: const TextStyle(fontFamily: 'NotoSans'),
+          titleMedium: const TextStyle(fontFamily: 'NotoSans'),
+          bodyLarge: const TextStyle(fontFamily: 'NotoSans'),
+          bodyMedium: const TextStyle(fontFamily: 'NotoSans'),
+          titleSmall: const TextStyle(fontFamily: 'NotoSans'),
+          labelSmall: const TextStyle(fontFamily: 'NotoSans'),
+        ),
+        colorScheme: ColorScheme.fromSwatch(primarySwatch: Colors.deepPurple)
+            .copyWith(secondary: Colors.orange),
       ),
-      home: new LoginPage()
+      navigatorObservers: [TransitionRouteObserver()],
+      initialRoute: LoginScreen.routeName,
+      routes: {
+        LoginScreen.routeName: (context) => const LoginScreen(),
+        DashboardScreen.routeName: (context) => const DashboardScreen(),
+      },
     );
   }
 }
